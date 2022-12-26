29501b47302b38a179d4b9444d12423b47f85da8
Fedor Abrashin <abrashin012@gmail.com>
2022-12-26 18:47:13 +0300

    trying UA

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -54,10 +54,14 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-        gtag: {
-          trackingID: 'G-DQGSK1RV7G',
+        googleAnalytics: {
+          trackingID: 'UA-2163144-25',
           anonymizeIP: true,
         },
+        // gtag: {
+        //   trackingID: 'G-DQGSK1RV7G',
+        //   anonymizeIP: true,
+        // },
       }),
     ],
   ],
