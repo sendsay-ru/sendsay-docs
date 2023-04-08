ff8ee9d9323980db015d700ed4322eb6c7ae4d04
Matveev89 <matveev@iprojects.ru>
2023-04-08 14:09:32 +0300

    feat(sensay-docs): add gtag and yandex metrika plugin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,9 +3,52 @@
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const analyticsId = process.env.GTM_ANALYTICS_ID;
-const scriptsId = process.env.GTM_SCRIPTS_ID;
+const baseGTM = process.env.GTM_SCRIPTS_ID;
+const landingGTM = process.env.GTM_LANDING_GROUP_COUNTER_ID;
+const yandexMetricaCounter = process.env.YANDEX_METRICA_COUNTER_ID;
+
+const gtagIds = [baseGTM, landingGTM].filter((id) => Boolean(id));
+
+const plugins = [
+  [
+    'docusaurus-lunr-search',
+    {
+      languages: ['ru', 'en'],
+      excludeRoutes: require('./config/searchExcludeRoutes.json'),
+    },
+  ],
+  [
+    '@docusaurus/plugin-client-redirects',
+    {
+      redirects: require('./config/redirects.json'),
+    },
+  ],
+  [
+    '@docusaurus/plugin-google-analytics',
+    {
+      trackingID: analyticsId,
+      anonymizeIP: true,
+    },
+  ],
+];
+
+if (yandexMetricaCounter) {
+  plugins.push([
+    'docusaurus-plugin-yandex-metrica',
+    {
+      counterID: yandexMetricaCounter,
+    },
+  ]);
+}
+
+if (gtagIds?.length) {
+  plugins.push(['@docusaurus/plugin-google-gtag', { trackingID: gtagIds, anonymizeIP: true }]);
+}
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -27,33 +70,13 @@ const config = {
     locales: ['ru', 'en'],
   },
 
-  plugins: [
-    [
-      'docusaurus-lunr-search',
-      {
-        languages: ['ru', 'en'],
-        excludeRoutes: require('./config/searchExcludeRoutes.json'),
-      },
-    ],
-    [
-      '@docusaurus/plugin-client-redirects',
-      {
-        redirects: require('./config/redirects.json'),
-      },
-    ],
-    [
-      'docusaurus-plugin-yandex-metrica',
-      {
-        counterID: '5230714',
-      },
-    ],
-  ],
+  plugins,
 
   presets: [
     [
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      {
         docs: {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
@@ -63,23 +86,16 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-        gtag:
-          analyticsId || scriptsId
-            ? {
-                trackingID: [analyticsId, scriptsId],
-                anonymizeIP: true,
-              }
-            : undefined,
         sitemap: {
           ignorePatterns: require('./config/sitemapIgnorePatterns'),
         },
-      }),
+      },
     ],
   ],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
+    {
       navbar: {
         logo: {
           alt: 'Sendsay docs',
@@ -127,7 +143,7 @@ const config = {
       colorMode: {
         disableSwitch: true,
       },
-    }),
+    },
 };
 
 module.exports = config;
