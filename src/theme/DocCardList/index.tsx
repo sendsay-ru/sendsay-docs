4b91caeaf04aebb08528a4471135da84fbcdcf33
Matveev89 <matveev@iprojects.ru>
2023-05-19 17:47:19 +0300

    feat(sendsay-docs): release notes page

--- src/theme/DocCardList/index.tsx
+++ src/theme/DocCardList/index.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import DocCardList from '@theme-original/DocCardList';
 import { PropSidebarItem } from '@docusaurus/plugin-content-docs';
+import { useCurrentSidebarCategory } from '@docusaurus/theme-common';
 import { getAllowedCardItems } from '../utils';
+import styles from './styles.module.css';
 
 type Props = {
   items: PropSidebarItem[];
   className: string;
 };
 
-const DocCardListWrapper = ({ items = [], ...restProps }: Props): JSX.Element => {
+const DocCardListWrapper = ({ ...restProps }: Props): JSX.Element => {
+  const { items } = useCurrentSidebarCategory();
+
   const filteredItems = getAllowedCardItems(items);
 
-  return <DocCardList {...restProps} items={filteredItems} />;
+  return (
+    <DocCardList {...restProps} items={filteredItems} className={styles[restProps.className]} />
+  );
 };
 
 export default DocCardListWrapper;
