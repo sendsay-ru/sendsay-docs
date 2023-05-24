e2765fd14acba037fbf9fd2b59058f934b85b9bb
Matveev89 <matveev@iprojects.ru>
2023-05-24 14:43:40 +0300

    fix

--- src/theme/constants.ts
+++ src/theme/constants.ts
@@ -1,2 +1,2 @@
 export const RESCTRICTED_ACCESS_KEY = 'HAS_ACCESS';
-export const HIDDEN_CATEGORIES_LABELS = ['en', 'integrations', 'videolessons'];
+export const HIDDEN_CATEGORIES_LABELS = ['en', 'Интеграции с приложениями', 'Видеоуроки'];
