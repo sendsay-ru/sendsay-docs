b8be4ac879b6e39a4016fe539532d30bd4177550
Aleksey Nikolaev <nikolaev@iprojects.ru>
2023-11-07 16:21:07 +0300

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ import 'dotenv/config';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './e2e',
+  testDir: process.env.PUBLISH ? './scripts' : './e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
